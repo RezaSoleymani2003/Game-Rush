21e7e0e4097f75ae8dd2856c153bae065c9abc85
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-03-19 13:40:36 +0330

    fix: urls

--- front/pages/xo.js
+++ front/pages/xo.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://game-rush-production.up.railway.app:8765");
+const socket = new WebSocket("ws://game-rush-production.up.railway.app");
 const board = document.getElementById("board");
 const popup = document.getElementById("popup");
 const mode = document.getElementById("mode");
