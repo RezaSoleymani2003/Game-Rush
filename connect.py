11df71249165204ef5f0337463e17b9685a1911c
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-03-22 09:02:49 +0430

    feat: base game logic

--- connect.py
+++ connect.py
@@ -1,15 +1,49 @@
 class Connect4:
     
     def __init__(self):
-        self.grid = [['0' for i in range(7)] for j in range(6)]
+        self.board = [['0' for i in range(7)] for j in range(6)]
 
 
     def update_board(self, symbol, i , j):
         self.step += 1
         self.board[i][j] = symbol
 
-    
-    
+    def check_winner(board):
+        get_winner = {"XXXX" : "X", "OOOO" : "O"}
+
+        for i in range(6):
+            for j in range(4):
+                line = ''.join([board[i][j], board[i][j+1], board[i][j+2], board[i][j+3]])
+
+                val = get_winner.get(line)
+                if val != None:
+                    return val
+
+        for j in range(7):
+            for i in range(3):
+                line = ''.join([board[i][j], board[i+1][j], board[i+2][j], board[i+3][j]])
+
+                val = get_winner.get(line)
+                if val != None:
+                    return val
+
+        for i in range(3):
+            for j in range(4):
+                line = ''.join([board[i][j], board[i+1][j+1], board[i+2][j+2], board[i+3][j+3]])
+
+                val = get_winner.get(line)
+                if val != None:
+                    return val
+
+        for i in range(0,3):
+            for j in range(3,7):
+                line = ''.join([board[i][j], board[i+1][j-1], board[i+2][j-2], board[i+3][j-3]])
+
+                val = get_winner.get(line)
+                if val != None:
+                    return val
+              
+        return "0"
 
 
 
@@ -17,4 +51,11 @@ class Connect4:
 
 connect4 = Connect4()
 
-print(connect4.grid)
+connect4.board = [['0', '0', '0', '0', '0', 'X', 'X'], 
+                  ['X', '0', '0', 'X', '0', '0', '0'], 
+                  ['X', '0', '0', 'X', 'X', '0', 'X'], 
+                  ['0', 'X', 'X', '0', '0', 'X', 'X'], 
+                  ['0', '0', 'X', '0', '0', '0', '0'], 
+                  ['X', '0', '0', 'X', 'X', '0', 'X']]
+
+print(Connect4.check_winner(connect4.board))
