4467dfce2342aa20c5c60a5263b04a1071e34f07
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-03-22 07:09:14 +0430

    Revert "fix: urls"
    This reverts commit a7fa6c89d4c8323bcf848db0824c75ec44445459.

--- back/server.py
+++ back/server.py
@@ -146,9 +146,7 @@ async def handle_connection(websocket, path):
     except Exception as e:
         print(f"Connection handler failed: {e}", flush=True)
 
-PORT = int(os.getenv("PORT", 443))
-
-start_server = websockets.serve(handle_connection, "0.0.0.0", PORT)
+start_server = websockets.serve(handle_connection, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
