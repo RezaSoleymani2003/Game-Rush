c667443655e313a519e21cc205493b93b815575a
Reza Soleymani <106319850+RezaSoleymani2003@users.noreply.github.com>
2025-03-24 09:47:56 +0430

    fix: returned to local

--- back/server.py
+++ back/server.py
@@ -5,8 +5,6 @@ import os
 import time
 from games.XO import XO, XO_bot
 
-PORT = int(os.getenv("PORT", 8765))  
-
 async def handle_xo_ai_mode(player_ws):
     mapper = {"easy": "random", "medium": "rule based", "hard": "minimax"}
     xo = XO()
@@ -147,7 +145,7 @@ async def handle_connection(websocket, path):
     except Exception as e:
         print(f"Connection handler failed: {e}", flush=True)
 
-start_server = websockets.serve(handle_connection, "0.0.0.0", PORT)
+start_server = websockets.serve(handle_connection, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
